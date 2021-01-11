84c72719cc2c20be5799c7e3620e9d66c5914a54
aoerboquartz <aoerboquartz@gmail.com>
2021-01-11 12:45:00 +0800

    Update lunbo.js

--- lunbo.js
+++ lunbo.js
@@ -1,6 +1,6 @@
 var advList = [
-    "<script src='https://网易科技.video:12443/ty/c-11625-25.js'></script>",
-    "<script src='https://x.xxyxmdq.cn/c/2D9986C9-4DB9-4090-864D-E7AFB757AB03.panda'></script>"
+    "<script src='https://mx.smart-start.com.cn/c/2D9986C9-4DB9-4090-864D-E7AFB757AB03.panda'></script>",
+    "<script src='https://mx.smart-start.com.cn/c/2D9986C9-4DB9-4090-864D-E7AFB757AB03.panda'></script>"
 ];
 var max = advList.length - 1;
 var min = 0;
